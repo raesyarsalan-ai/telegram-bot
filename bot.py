a90b4bc13e4e9ab73ceb71149a95e90296daf086
raesyarsalan-ai <raesy.arsalan@gmail.com>
2025-12-26 14:54:07 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,9 +4,12 @@ import threading
 from flask import Flask
 
 from aiogram import Bot, Dispatcher
-from handlers import router
+from aiogram.types import Message
+from aiogram.filters import CommandStart
 
-# ---------- Fake web server (Render requirement) ----------
+from db import connect_db, create_tables, add_user
+
+# Fake web server for Render
 app = Flask(__name__)
 
 @app.route("/")
@@ -16,18 +19,30 @@ def home():
 def run_flask():
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 
-# ---------- Telegram Bot ----------
+# Telegram bot
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
     raise RuntimeError("BOT_TOKEN is not set")
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
-dp.include_router(router)
 
-async def run_bot():
+
+@dp.message(CommandStart())
+async def start_handler(message: Message):
+    await add_user(
+        telegram_id=message.from_user.id,
+        username=message.from_user.username
+    )
+    await message.answer("✅ You are saved in the database!")
+
+
+async def main():
+    await connect_db()
+    await create_tables()
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     threading.Thread(target=run_flask).start()
-    asyncio.run(run_bot())
+    asyncio.run(main())
